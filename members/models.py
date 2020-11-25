cb44d8e863c1166c8cae6dc0429a6072a695115b
mkafrawy <m.kafrawy@magmaksa.com>
2020-11-25 16:12:50 +0200

    add member model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+MEMBER_TYPES = (
+    ('Normal', 'Normal'),
+    ('Active', 'Active'),
+    ('Honer', 'Honer'),
+)
+GENDER = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Other', 'Other'),
+)
+
+
+class Members(models.Model):
+    member_no = models.CharField(max_length=25)
+    member_name = models.CharField(max_length=200)
+    id_no = models.CharField(max_length=25)
+    created_date = models.DateTimeField(auto_now=True)
+    date_of_birth = models.DateField()
+    member_type = models.CharField(max_length=25, choices=MEMBER_TYPES)
+    gender = models.CharField(max_length=25, choices=GENDER)
+
+    def __str__(self):
+        return self.member_name
