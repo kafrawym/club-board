b1d5a17a2dcbb190af713e94ee58de0847b89402
mkafrawy <m.kafrawy@magmaksa.com>
2020-12-09 16:11:56 +0200

    add accounts -- update 09-12-2020

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('members/', include('members.urls', namespace='members')),
 ]
